4c0bd006eb2aa5080e780c435b7f32c7b9eed602
Zynkah <ffgurl07@yahoo.com>
2022-09-24 12:34:35 -0700

    Week 1 Workshop

---
+++ src/features/campsites/CampsiteCard.js
@@ -0,0 +1,18 @@
+import {Card, CardImg, CardImgOverlay, CardTitle}  from "reactstrap";
+
+
+const CampsiteCard = (props) => {
+  return (
+    <Card>
+      <CardImg
+        width="100%"
+        src={props.campsite.image}
+        alt={props.campsite.name}
+      />
+      <CardImgOverlay />
+      <CardTitle>{props.campsite.name}</CardTitle>
+    </Card>
+  );
+};
+
+export default CampsiteCard;
