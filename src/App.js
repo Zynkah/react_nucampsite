bf44db55440ee7bdb3b397dd0d2cf5c3f881174b
Zynkah <ffgurl07@yahoo.com>
2022-10-12 12:26:59 -0700

    PartnersList update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
 import "./App.css";
-import AboutPage from './pages/AboutPage';
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='contact' element={<ContactPage />} />
-        <Route path='directory' element={<CampsitesDirectoryPage />} />
-        <Route
-          path='directory/:campsiteId'
-          element={<CampsiteDetailPage />}
-          />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
       </Routes>
       <Footer />
     </div>
--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -1,5 +1,5 @@
 import { Col } from "reactstrap";
-import Partner from "./partner";
+import Partner from "./Partner";
 import { selectAllPartners } from './partnersSlice';
 
 const PartnersList = () => {
