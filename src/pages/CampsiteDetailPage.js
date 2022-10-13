232a598991f0a3c367dbe97b1a7c367e611e3976
Zynkah <ffgurl07@yahoo.com>
2022-10-12 18:49:35 -0700

    Redux for All

--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -2,7 +2,7 @@ import { Container, Row } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import { selectCampsiteById } from '../features/campsites/campsitesSlice';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
-import CommentsList from '../features/comments/Commentslist';
+import CommentsList from '../features/comments/CommentsList';
 import SubHeader from '../components/SubHeader';
 import { useSelector } from 'react-redux';
 
